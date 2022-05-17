e7cac308cc58b73b4121de79917894a653a6bac3
ravindrapv <ravindra.ravi4849@gmail.com>
2022-05-17 15:05:57 +0530

    image search

--- block-BJaaju/code/index.js
+++ block-BJaaju/code/index.js
@@ -1,47 +1,40 @@
-const input = document.querySelector("input");
-const body = document.querySelector("body");
-const img = document.querySelectorAll('img');
-const div = document.querySelector('div');
-const box = document.querySelector('.box');
+const url = "https://api.unsplash.com/photos/?client_id=QhqvUONEWkaZXj6g964I_W4MDCGviaqzYsVogJ9tEIU";
+
+const searchUrl = (query) => `https://api.unsplash.com/search/photos?&query=${query}&client_id=QhqvUONEWkaZXj6g964I_W4MDCGviaqzYsVogJ9tEIU`;
+const root = document.querySelector('.images');
+const searchElm = document.querySelector('input');
 
 
+function fetch(url, successHandler){
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET',url);
+    xhr.onload = () => successHandler(JSON.parse(xhr.response));
 
-function handelChange(event){
-    if (event.keyCode === 13 ){
-    let XHR = new XMLHttpRequest();
-    let keyWord = event.target.value.toLowerCase();
-XHR.open('GET',`https://api.unsplash.com/search/photos?query=${keyWord}&client_id=ilGa2_Ql0bDszy8XXGlQ8RMxdLT5ZyfU4FFx0AbtM3c`);
-
-XHR.onload = function () {
-    let imgData = JSON.parse(XHR.response);
-    console.log(imgData)
-    data = [imgData];
-   
-    console.log(data[0].results)
-    let arr = [];
-    data[0].results.forEach((ele) => {
-        console.log(ele.urls.small);
-        arr.push(ele.urls.small);   
-    });
-    arr.forEach((v)=>{
-        box.style.padding = "30px";
-        div.append(box);
-        img.style.height = "300px";
-        img.style.width = "300px";
-        img.src = v;
-        box.append(img);
-    })
-    console.log(arr);
-};
-XHR.onerror = function () {
-    console.error("Somthing! Went Wrong");
-};
-
- XHR.send();
- event.target.value = " ";
- div.innerHTML = " ";
- }
+    xhr.onerror = function(){
+        console.error('somthing went wrong');
+    };
+    xhr.send();
+}
+function displayImages(images){
+        root.innerHTML = '';
+        images.forEach(image => {
+            let li = document.createElement('li');
+            let img = document.createElement('img');
+            img.src = image.urls.thumb
+            li.append(img);
+            root.append(li);
+        });
 }
 
-input.addEventListener('keyup',handelChange);
-console.log("script is working!!!")
+fetch(url, displayImages);
+function handelsearch(event){
+    if(event.keyCode == 13 && searchElm.value){
+        fetch(searchUrl(searchElm.value), (searchResult) =>{
+            displayImages(searchResult.results);
+        });
+        searchElm.val = "";
+    }
+}
+
+
+searchElm.addEventListener('keyup', handelsearch);
