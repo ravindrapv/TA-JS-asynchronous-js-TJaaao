788b71c8e83418494edc21b9783cff5d180dbca3
ravindrapv <ravindra.ravi4849@gmail.com>
2022-05-17 12:46:49 +0530

    Assignmnet: github user finder

--- block-BJaajt/code/index.js
+++ block-BJaajt/code/index.js
@@ -1,31 +1,59 @@
-let userName = document.querySelector("h2");
-let avathar = document.querySelector("img");
-let fallower = document.querySelector("p");
-let fallowing = document.querySelector("h3");
-let input = document.querySelector("input");
+const img = document.querySelector(".profile");
+const userName = document.querySelector("h3");
+const company = document.querySelector("p");
+const followers = document.querySelector(".followers");
+const following = document.querySelector(".following");
+const input = document.querySelector("input");
+const foll = document.querySelector("foll");
+const body = document.querySelector('body');
 
-function CreatUi(data) {
-  avathar.src = data.avatar_url;
-  userName.innerText = data.userName;
-  fallower.innerText = `following: ${data.fallower}`;
-  fallowing.innerText = `falloewr: ${data.fallowing}`;
+function displayUI(data){
+    img.src = data.avatar_url;
+    userName.innerText = data.name;
+    company.innerText = data.company;
+    followers.innerText = `Followers: ${data.followers}`;
+    following.innerText = `Following: ${data.following}`;
+            
 }
 
-function handelChange() {
-  if (event.keyCode === 13) {
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET",`https://api.github.com/user/${event.target.value}`);
-    xhr.onload = function () {
-      let userData = JSON.parse(xhr.response);
-      CreatUi(userData);
-    };
-    xhr.onerror = function () {
-      console.log("somthing went wrong");
-    };
-    xhr.send();
-    event.target.value = '';
-  }
+function handelChange(event){
+    if (event.keyCode === 13 ){
+        let XHR = new XMLHttpRequest();
+       XHR.open('GET',`https://api.github.com/users/${event.target.value}`);
+       XHR.onload = function(){
+            let userData = JSON.parse(XHR.response);
+            var arr = userData
+
+            let fl = arr.followers_url;
+            console.log(fl)
+            
+        let XHR1 = new XMLHttpRequest();
+            XHR1.open('GET',`${fl}`);
+            XHR1.onload = function(){
+                let userFollow = JSON.parse(XHR1.response);
+                console.log(userFollow);
+                userFollow.forEach((e)=>{
+                    console.log(e.login)
+                    let rImg = document.createElement("img");
+                    rImg.classList.add("round");
+                    console.log(rImg)
+                    rImg.src = e.avatar_url;
+                    
+                    body.append(rImg);
+                })
+                
+        }
+            XHR1.send();
+            displayUI(userData);
+        };
+        XHR.onerror = function(){
+            console.error('somthing went wrong');
+        };
+        XHR.send();
+        event.target.value = '';
+        
+        
+    }
 }
 
-
-input.addEventListener("keyup", handelChange);
+input.addEventListener('keyup',handelChange);
